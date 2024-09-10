6892657b2db52db664ea792d1a91f4aef3d59aee
saadbasheer <saadbasheer0@gmail.com>
2024-09-10 10:37:09 +0530

    tooltip

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,8 +3,17 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <small className="text-gray-500 text-nowrap  z-5 ">
-        Designed and developed by <span className="underline hover:text-primary"><a href="https://x.com/sd0br">saadb</a></span>
+      <small className="text-gray-500 text-nowrap z-5">
+        Designed and developed by{" "}
+        <span className="underline hover:text-primary">
+          <a
+            href="https://x.com/sd0br"
+            title="come say hi!"
+            className="hover:underline"
+          >
+            saadb
+          </a>
+        </span>
       </small>
     </div>
   );
